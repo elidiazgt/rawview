60a6a1ce13c57b6f0865a500146eecb32e3427d8
Eli Diaz <elidiaz.gt@gmail.com>
2015-07-15 00:42:02 -0600

    Created data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+data = {
+ "link_list": [
+  {
+    "title": "Javascript",
+    "url": "url",
+    "description": "Demo"    
+  },
+  {
+    "title": "Javascript",
+    "url": "url",
+    "description": "Demo"    
+  }
+  
+]}
