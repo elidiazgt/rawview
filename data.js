1854ccead7bff93136c243259cc223d63710e8ad
gamexcopy <celestialveing@gmail.com>
2015-07-16 19:16:17 -0600

    añadimos archivos css

--- data.js
+++ data.js
@@ -1,15 +1,20 @@
-hola = 100;
 data = {
- "link_list": [
+"global_title":"Este es un gran titulo",
+"lorem":"Lorem ipsum dolor sit amet, labitur neglegentur nam ad, vel quis iisque referrentur te. Solet conclusionemque ne eam, ea mea fastidii eligendi definitionem. Ne nostrum quaestio his. Usu ne nibh appetere urbanitas, quando habemus consequuntur eum no.",
+
+
+ "info": [
   {
-    "title": "Josue"+(8*9+1+5)+hola,
-    "url": "url",
+    "title": "Josue",
     "description": "Demo"
   },
   {
-    "title": "Javascript"+hola,
-    "url": "url",
+    "title": "Josue",
+    "description": "Demo"
+  },
+  {
+    "title": "Josue",
     "description": "Demo"
   }
-
-]}
+]
+}
